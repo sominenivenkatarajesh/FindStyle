3404bf62707b3148e71e9b8b5e475df6885187f1
manikanta rajesh <venkatrajeshnaidu@gmail.com>
2026-03-14 00:13:09 +0530

    Final fix for static file serving on Vercel

--- store/settings.py
+++ store/settings.py
@@ -70,10 +70,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "app/static",
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"  # Added STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # WhiteNoise storage for efficient static file delivery
 STORAGES = {
